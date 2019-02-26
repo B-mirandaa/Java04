4e90ae2cd52b1e19964e6d3f3618afee52c1fb36
BRUNO MIRANDA COSTA <BRUNO.MIRANDA@csi.aneas.msft>
2019-02-26 20:21:58 -0300

    java04

---
+++ src/TestaWhile.java
@@ -0,0 +1,12 @@
+
+public class TestaWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String "oi" + 5;
+		System.out.println("oi");
+
+	}
+
+}
---
+++ src/TesteCondicional.java
@@ -0,0 +1,24 @@
+
+public class TesteCondicional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int idade = 18;
+		int quantaPessoas = 3;
+		
+		if(idade) = 20;
+		int qtdPessoas = 2;
+		
+		if(idade) >= 18 || qtspessoas
+		 
+		
+		System.out.println(idade);
+		
+		
+		
+		
+		
+	}
+
+}
